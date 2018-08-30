e072b2af6b4976200c6b8afbc955ab995940e77b
Aleksandr Dankovtsev <adankovtsev@avito.ru>
2018-08-30 12:22:31 +0300

    add entrypoing

---
+++ src/cmd/main/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"net/http"
+	"time"
+	"fmt"
+)
+
+func main() {
+	handler := http.HandlerFunc(func (resp http.ResponseWriter, req *http.Request) {
+		// nothing
+	})
+
+	s := &http.Server{
+		Addr: ":42234",
+		Handler: handler,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: http.DefaultMaxHeaderBytes,
+	}
+
+	err := s.ListenAndServe()
+	if err != nil {
+		fmt.Sprintf("error in start application: %v", err)
+	}
+
+	fmt.Sprintf("application terminated")
+}
