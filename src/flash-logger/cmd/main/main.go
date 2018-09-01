e6160a495034d90d63f43b0e98230113c162a1b5
Aleksandr Dankovtsev <adankovtsev@avito.ru>
2018-09-01 11:19:19 +0300

    changes

--- src/flash-logger/cmd/main/main.go
+++ src/flash-logger/cmd/main/main.go
@@ -2,33 +2,19 @@ package main
 
 import (
 	"net/http"
-	"time"
-	"fmt"
+	"log"
 
 	"flash-logger/api/v1/event"
 )
 
 func main() {
-	fmt.Println("Starting application ...")
-
-	handler := http.HandlerFunc(func (resp http.ResponseWriter, req *http.Request) {
-		// nothing
-	})
+	log.Println("Starting application ...")
 
 	http.Handle("/event", event.New())
 
-	s := &http.Server{
-		Addr: ":42234",
-		Handler: handler,
-		ReadTimeout:    10 * time.Second,
-		WriteTimeout:   10 * time.Second,
-		MaxHeaderBytes: http.DefaultMaxHeaderBytes,
+	if err := http.ListenAndServe(":42234", nil); err != nil {
+		log.Fatalf("error in start application: %v", err)
 	}
 
-	err := s.ListenAndServe()
-	if err != nil {
-		fmt.Printf("error in start application: %v", err)
-	}
-
-	fmt.Printf("application terminated")
+	log.Printf("application terminated")
 }
