ece15ea88919e013f8dc4ecb828ab5085f7004c9
Aleksandr Dankovtsev <adankovtsev@avito.ru>
2018-09-02 23:50:05 +0300

    updat

--- src/flash-logger/storage/memory/get_last_messages.go
+++ src/flash-logger/storage/memory/get_last_messages.go
@@ -5,5 +5,26 @@ import (
 )
 
 func (s *Storage) GetLastMessages(projectID int, limit int, offset int) []model.Message {
-	return nil
+	messages := make([]model.Message, 0, limit)
+	count := 0
+	for _, tuple := range s.tuples {
+		if projectID == tuple.ProjectID {
+			count++
+			messages = append(
+				messages,
+				model.Message{
+					ID: tuple.Message.ID,
+					Level: tuple.Message.Level,
+					Message: tuple.Message.Message,
+					Context: tuple.Message.Context,
+					Tags: tuple.Message.Tags,
+				},
+			)
+		}
+
+		if count >= limit {
+			break
+		}
+	}
+	return messages
 }
